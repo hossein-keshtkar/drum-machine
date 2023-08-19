39dac7eada6d44b736af5baddf92d2d6b78769f5
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-19 17:55:58 -0400

    playing audios with keys

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import "./App.css";
 
 function App() {
   const [pressedKey, setPressedKey] = React.useState("");
-  const display = React.useRef();
+  const refs = {
+    display: React.useRef(),
+    Q: React.useRef(),
+    W: React.useRef(),
+    E: React.useRef(),
+    A: React.useRef(),
+    S: React.useRef(),
+    D: React.useRef(),
+    Z: React.useRef(),
+    X: React.useRef(),
+    C: React.useRef(),
+  };
+
   const instruments = {
     Q: "Heater-1",
     W: "Heater-2",
@@ -26,30 +38,33 @@ function App() {
   });
 
   React.useEffect(() => {
-    if (/[QWEASDZXC]/.test(pressedKey))
-      display.current.innerHTML = instruments[pressedKey];
+    if (/[QWEASDZXC]/.test(pressedKey)) {
+      refs.display.current.innerHTML = instruments[pressedKey];
+      refs[pressedKey].current.click();
+      refs[pressedKey].current.children[0].play();
+    }
   }, [pressedKey]);
 
   return (
     <div className="App">
       <div id="drum-machine">
-        <div id="display" ref={display}></div>
+        <div id="display" ref={refs.display}></div>
         <div className="row">
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.Q}>
             Q
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
               id="Q"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.W}>
             W
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
               id="W"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.E}>
             E
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
@@ -58,21 +73,21 @@ function App() {
           </button>
         </div>
         <div className="row">
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.A}>
             A
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
               id="A"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.S}>
             S
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
               id="S"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.D}>
             D
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
@@ -81,21 +96,21 @@ function App() {
           </button>
         </div>
         <div className="row">
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.Z}>
             Z
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
               id="Z"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.X}>
             X
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
               id="X"
             ></audio>
           </button>
-          <button className="drum-pad">
+          <button className="drum-pad" ref={refs.C}>
             C
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
