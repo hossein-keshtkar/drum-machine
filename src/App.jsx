3e5b7b603b867c07d85d5f37ec1bfe5977c96bd3
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-19 18:55:47 -0400

    optimizing effects

--- src/App.jsx
+++ src/App.jsx
@@ -33,87 +33,106 @@ function App() {
     setPressedKey(e.code.replace(/^Key/, ""));
   };
 
-  React.useEffect(() => {
-    document.addEventListener("keydown", keydownHandler);
-  });
+  const clickHandle = (e) => {
+    refs.display.current.innerHTML = instruments[e.target.children[0].id];
+    e.target.children[0].play();
+    e.target.className += " clicked";
+
+    setTimeout(() => {
+      e.target.className = "drum-pad";
+    }, 100);
+  };
 
   React.useEffect(() => {
+    window.addEventListener("keydown", keydownHandler);
+
     if (/[QWEASDZXC]/.test(pressedKey)) {
       refs.display.current.innerHTML = instruments[pressedKey];
       refs[pressedKey].current.click();
       refs[pressedKey].current.children[0].play();
     }
-  }, [pressedKey]);
+
+    return () => window.removeEventListener("keydown", keydownHandler);
+  });
 
   return (
     <div className="App">
       <div id="drum-machine">
         <div id="display" ref={refs.display}></div>
         <div className="row">
-          <button className="drum-pad" ref={refs.Q}>
+          <button className="drum-pad" ref={refs.Q} onClick={clickHandle}>
             Q
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+              onEnded={() => setPressedKey("")}
               id="Q"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.W}>
+          <button className="drum-pad" ref={refs.W} onClick={clickHandle}>
             W
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+              onEnded={() => setPressedKey("")}
               id="W"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.E}>
+          <button className="drum-pad" ref={refs.E} onClick={clickHandle}>
             E
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+              onEnded={() => setPressedKey("")}
               id="E"
             ></audio>
           </button>
         </div>
         <div className="row">
-          <button className="drum-pad" ref={refs.A}>
+          <button className="drum-pad" ref={refs.A} onClick={clickHandle}>
             A
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+              onEnded={() => setPressedKey("")}
               id="A"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.S}>
+          <button className="drum-pad" ref={refs.S} onClick={clickHandle}>
             S
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+              onEnded={() => setPressedKey("")}
               id="S"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.D}>
+          <button className="drum-pad" ref={refs.D} onClick={clickHandle}>
             D
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+              onEnded={() => setPressedKey("")}
               id="D"
             ></audio>
           </button>
         </div>
         <div className="row">
-          <button className="drum-pad" ref={refs.Z}>
+          <button className="drum-pad" ref={refs.Z} onClick={clickHandle}>
             Z
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+              onEnded={() => setPressedKey("")}
               id="Z"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.X}>
+          <button className="drum-pad" ref={refs.X} onClick={clickHandle}>
             X
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+              onEnded={() => setPressedKey("")}
               id="X"
             ></audio>
           </button>
-          <button className="drum-pad" ref={refs.C}>
+          <button className="drum-pad" ref={refs.C} onClick={clickHandle}>
             C
             <audio
               src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+              onEnded={() => setPressedKey("")}
               id="C"
             ></audio>
           </button>
