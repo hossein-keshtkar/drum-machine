1b94e9cb0586c6b047d4caafd29bd10b65156056
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-30 21:35:31 -0400

    new constant keys added

--- src/constants/keywords.js
+++ src/constants/keywords.js
@@ -1,1 +1,3 @@
 export const KEY_DOWN = "keydown";
+export const COMMA = "Comma";
+export const PERIOD = "Period";
--- src/funcs/keydownHandler.js
+++ src/funcs/keydownHandler.js
@@ -1,13 +1,14 @@
+import { COMMA, PERIOD } from "../constants/keywords";
+
 export const keydownHandler = (e, callback) => {
-  console.log(e.code);
   const regEx = /^Key/;
 
   if (regEx.test(e.code)) {
     const replacement = e.code.replace(regEx, "");
     callback(replacement);
-  } else if (e.code === "Comma") {
+  } else if (e.code === COMMA) {
     callback(",");
-  } else if (e.code === "Period") {
+  } else if (e.code === PERIOD) {
     callback(".");
   }
 };
