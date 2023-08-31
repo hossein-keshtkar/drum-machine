b57b6f0f4afff63b6b19339269b0eb6c5455a35b
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-31 00:06:49 -0400

    mode added to state

--- src/constants/keywords.js
+++ src/constants/keywords.js
@@ -3,3 +3,5 @@ export const COMMA = "Comma";
 export const PERIOD = "Period";
 export const VOLUME = "VOLUME";
 export const DISPLAY = "DISPLAY";
+export const LIGHT = "LIGHT";
+export const DARK = "DARK";
--- src/data/initState.js
+++ src/data/initState.js
@@ -1,4 +1,7 @@
+import { LIGHT } from "../constants/keywords";
+
 export const initState = {
   volume: 0.5,
   display: "",
+  mode: LIGHT,
 };
